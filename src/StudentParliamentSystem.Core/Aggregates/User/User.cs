e2d53fe3ed6e68d4067a240daa82b97384bb4a83
pichus <dimatyzuk@gmail.com>
2026-02-12 23:28:51 +0200

    fix: fix warnings in user entity

--- src/StudentParliamentSystem.Core/Aggregates/User/User.cs
+++ src/StudentParliamentSystem.Core/Aggregates/User/User.cs
@@ -6,13 +6,13 @@ namespace StudentParliamentSystem.Core.Aggregates.User;
 public class User
 {
     public Guid Id { get; init; }
-    public string Name { get; set; }
+    public required string Name { get; set; }
     public DateTime CreatedAtUtc { get; init; }
 
-    public ICollection<Role.Role> Roles { get; set; }
-    public ICollection<Department.Department> Departments { get; set; }
-    public ICollection<DepartmentMember> DepartmentMembers { get; set; }
-    public ICollection<Event.Event> CreatedEvents { get; set; }
-    public ICollection<EventRegistration> EventRegistrations { get; set; }
-    public ICollection<OrganizationRequest.OrganizationRequest> OrganizationRequests { get; set; }
+    public ICollection<Role.Role> Roles { get; set; } = [];
+    public ICollection<Department.Department> Departments { get; set; } = [];
+    public ICollection<DepartmentMember> DepartmentMembers { get; set; } = [];
+    public ICollection<Event.Event> CreatedEvents { get; set; } = [];
+    public ICollection<EventRegistration> EventRegistrations { get; set; } = [];
+    public ICollection<OrganizationRequest.OrganizationRequest> OrganizationRequests { get; set; } = [];
 }
