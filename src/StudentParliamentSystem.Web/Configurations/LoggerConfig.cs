f5f778c97619a47136c90a3f7b2f2baddc787a49
pichus <dimatyzuk@gmail.com>
2026-02-19 23:30:29 +0200

    feat(web): configure logging

---
+++ src/StudentParliamentSystem.Web/Configurations/LoggerConfig.cs
@@ -0,0 +1,19 @@
+using Serilog;
+
+namespace StudentParliamentSystem.Api.Configurations;
+
+public static class LoggerConfig
+{
+    public static IServiceCollection AddLoggerConfig(this IServiceCollection services, IConfiguration configuration,
+        IWebHostEnvironment environment)
+    {
+        services.AddSerilog(new LoggerConfiguration()
+            .ReadFrom.Configuration(configuration)
+            .Enrich.FromLogContext()
+            .Enrich.WithProperty("Application", environment.ApplicationName)
+            .WriteTo.Console()
+            .CreateLogger());
+
+        return services;
+    }
+}
