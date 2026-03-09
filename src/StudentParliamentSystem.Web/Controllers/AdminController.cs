7ca5d601bb00c9897634dc890506ed2890a39906
pichus <dimatyzuk@gmail.com>
2026-03-09 21:49:54 +0200

    feat: protect admin panel

--- src/StudentParliamentSystem.Web/Controllers/AdminController.cs
+++ src/StudentParliamentSystem.Web/Controllers/AdminController.cs
@@ -1,7 +1,11 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+
+using StudentParliamentSystem.Api.Configurations;
 
 namespace StudentParliamentSystem.Api.Controllers;
 
+[Authorize(Policy = AuthorizationPolicyNameConstants.CanAccessAdminPanel)]
 public class AdminController : Controller
 {
     // GET: /Admin
@@ -15,5 +19,20 @@ public class AdminController : Controller
     public IActionResult Users()
     {
         return View();
+    }
+
+    // GET: /Admin/Edit/5
+    public IActionResult Edit(string id)
+    {
+        return View();
+    }
+
+    // POST: /Admin/Edit/5
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public IActionResult Edit(string id, IFormCollection collection)
+    {
+        // For demonstration, simply redirect back to the user list.
+        return RedirectToAction(nameof(Users));
     }
 }
