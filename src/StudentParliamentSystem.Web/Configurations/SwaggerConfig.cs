b984826ff282115e4c141bf0fd6affd929dc6133
pichus <dimatyzuk@gmail.com>
2026-03-09 19:09:34 +0200

    add swagger config

---
+++ src/StudentParliamentSystem.Web/Configurations/SwaggerConfig.cs
@@ -0,0 +1,27 @@
+namespace StudentParliamentSystem.Api.Configurations;
+
+public static class SwaggerConfig
+{
+    public static IServiceCollection AddSwaggerConfig(this IServiceCollection services)
+    {
+        services.AddOpenApiDocument(document =>
+        {
+            // to enable dropdowns for nullable enums in swagger ui
+            document.SchemaSettings.AllowReferencesWithProperties = true;
+            document.SchemaSettings.GenerateEnumMappingDescription = true;
+
+            // document.AddSecurity("Bearer", [], new OpenApiSecurityScheme
+            // {
+            //     Type = OpenApiSecuritySchemeType.Http,
+            //     Scheme = JwtBearerDefaults.AuthenticationScheme,
+            //     BearerFormat = "JWT",
+            //     Description = "Type into the textbox: {your JWT token}."
+            // });
+            //
+            // document.OperationProcessors.Add(
+            //     new AspNetCoreOperationSecurityScopeProcessor("Bearer"));
+        });
+
+        return services;
+    }
+}
--- src/StudentParliamentSystem.Web/Program.cs
+++ src/StudentParliamentSystem.Web/Program.cs
@@ -1,15 +1,11 @@
 using StudentParliamentSystem.Api.Configurations;
-using StudentParliamentSystem.Infrastructure;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Logging.ClearProviders();
 builder.Logging.AddConsole();
 
-builder.Services.AddInfrastructureServices(builder.Configuration,
-    builder.Environment.EnvironmentName);
-
-builder.Services.AddOpenApi();
+builder.Services.AddServices(builder.Environment, builder.Configuration);
 
 var app = builder.Build();
 
