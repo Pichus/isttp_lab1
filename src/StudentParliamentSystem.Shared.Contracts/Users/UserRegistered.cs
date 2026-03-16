d3bc4abf3a16e8b1f3700d6a706edbcf437bc55c
pichus <dimatyzuk@gmail.com>
2026-03-16 20:05:40 +0200

    feat(shared): copy roles from identity user to application user

--- src/StudentParliamentSystem.Shared.Contracts/Users/UserRegistered.cs
+++ src/StudentParliamentSystem.Shared.Contracts/Users/UserRegistered.cs
@@ -1,3 +1,3 @@
 namespace StudentParliamentSystem.Shared.Contracts.Users;
 
-public record UserRegistered(Guid UserId, string Email, string FirstName, string LastName);
+public record UserRegistered(Guid UserId, string Email, string FirstName, string LastName, ICollection<string> Roles);
