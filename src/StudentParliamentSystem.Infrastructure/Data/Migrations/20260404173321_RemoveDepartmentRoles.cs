4c2d69b370adb8e8fc6c5ab0dfbf9a9cb9749fa0
pichus <dimatyzuk@gmail.com>
2026-03-18 21:43:31 +0200

    feat(infra): remove department roles

---
+++ src/StudentParliamentSystem.Infrastructure/Data/Migrations/20260404173321_RemoveDepartmentRoles.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace StudentParliamentSystem.Infrastructure.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemoveDepartmentRoles : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DepartmentRoleId",
+                schema: "main_application",
+                table: "department_members");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "DepartmentRoleId",
+                schema: "main_application",
+                table: "department_members",
+                type: "uuid",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+        }
+    }
+}
