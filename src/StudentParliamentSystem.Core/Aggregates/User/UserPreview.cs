43a4d79413fd12b3371d62bfba531cfb805ea109
pichus <dimatyzuk@gmail.com>
2026-03-20 00:30:06 +0200

    feat(core): include departments in user preview

--- src/StudentParliamentSystem.Core/Aggregates/User/UserPreview.cs
+++ src/StudentParliamentSystem.Core/Aggregates/User/UserPreview.cs
@@ -5,5 +5,6 @@ public class UserPreview
     public required string FirstName { get; init; }
     public required string LastName { get; init; }
     public required string Email { get; init; }
+    public IEnumerable<string> Departments { get; init; } = [];
     public IEnumerable<string> Roles { get; init; } = [];
 }
