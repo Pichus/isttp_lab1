7f7763ba379e362e062416536be0cffc2d1453ec
pichus <dimatyzuk@gmail.com>
2026-03-31 01:54:31 +0300

    feat: a migration for changes in role entity

---
+++ src/StudentParliamentSystem.Infrastructure/Data/Migrations/20260414143753_FixNaming.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace StudentParliamentSystem.Infrastructure.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixNaming : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTimeOffset>(
+                name: "CreatedAt",
+                schema: "main_application",
+                table: "roles",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTimeOffset(new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 0, 0, 0, 0)));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CreatedAt",
+                schema: "main_application",
+                table: "roles");
+        }
+    }
+}
