f78983e389f350767c943d9147f07c0b0eb029b5
pichus <dimatyzuk@gmail.com>
2026-04-03 20:07:02 +0300

    feat(core): edit user functionality

--- src/StudentParliamentSystem.Core/Aggregates/User/IUserRepository.cs
+++ src/StudentParliamentSystem.Core/Aggregates/User/IUserRepository.cs
@@ -7,5 +7,6 @@ public interface IUserRepository
     void Add(User user);
     Task<bool> ExistsAsync(Guid userId);
     Task<bool> ExistsAsync(string email);
+    Task<User?> GetByIdAsync(Guid userId);
     Task<PagedResult<UserPreview>> RetrieveAllAsync(int pageNumber, int pageSize, string? query = null);
 }
--- src/StudentParliamentSystem.Core/Aggregates/User/UserPreview.cs
+++ src/StudentParliamentSystem.Core/Aggregates/User/UserPreview.cs
@@ -2,6 +2,7 @@ namespace StudentParliamentSystem.Core.Aggregates.User;
 
 public class UserPreview
 {
+    public Guid Id { get; init; }
     public required string FirstName { get; init; }
     public required string LastName { get; init; }
     public required string Email { get; init; }
