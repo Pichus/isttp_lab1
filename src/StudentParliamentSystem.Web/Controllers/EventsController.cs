a17e9b6db992153cf91795aca40d78e25ad47877
pichus <dimatyzuk@gmail.com>
2026-04-06 19:09:47 +0300

    feat(web): events page

--- src/StudentParliamentSystem.Web/Controllers/EventsController.cs
+++ src/StudentParliamentSystem.Web/Controllers/EventsController.cs
@@ -1,20 +1,20 @@
 using System.Security.Claims;
+
+using FluentResults;
+
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using StudentParliamentSystem.Api.Models.Events;
+using StudentParliamentSystem.Core.Abstractions;
 using StudentParliamentSystem.Core.Aggregates.Department;
 using StudentParliamentSystem.Core.Aggregates.Event;
 using StudentParliamentSystem.UseCases.Departments.Retrieve.ForUser;
 using StudentParliamentSystem.UseCases.Events.Create;
+using StudentParliamentSystem.UseCases.Events.Retrieve.ById;
+using StudentParliamentSystem.UseCases.Events.Retrieve.Published;
 using StudentParliamentSystem.UseCases.Events.Retrieve.Tags;
 using Wolverine;
-using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Mvc;
-using StudentParliamentSystem.Api.Models.Events;
-using StudentParliamentSystem.Core.Aggregates.Department;
-using StudentParliamentSystem.UseCases.Departments.Retrieve.ForUser;
-using StudentParliamentSystem.UseCases.Events.Create;
-using Wolverine;
+using FluentResults;
 
 namespace StudentParliamentSystem.Api.Controllers;
 
@@ -28,25 +28,34 @@ public class EventsController : Controller
         _bus = bus;
     }
 
-    // GET: /Events/
-    public IActionResult Index()
+    [HttpGet]
+    public async Task<IActionResult> Index(int page = 1, string? query = null, string? tag = null, string? sortOrder = "date_desc")
     {
-        return View();
+        var result = await _bus.InvokeAsync<PagedResult<EventPreview>>(
+            new RetrievePublishedEvents(page, 12, query, tag, sortOrder));
+
+        var tags = await _bus.InvokeAsync<IEnumerable<EventTag>>(new RetrieveAllEventTags());
+
+        ViewBag.Query = query;
+        ViewBag.Tag = tag;
+        ViewBag.SortOrder = sortOrder;
+        ViewBag.Tags = tags.Select(t => t.Name).ToList();
+
+        return View(result);
     }
 
-    // GET: /Events/Archive
-    public IActionResult Archive()
+    [HttpGet]
+    public async Task<IActionResult> Details(Guid id)
     {
-        return View();
+        var result = await _bus.InvokeAsync<Result<Event>>(new RetrieveEventById(id));
+        
+        if (result.IsFailed)
+            return NotFound();
+
+        return View(result.Value);
     }
 
-    // GET: /Events/Details/5
-    public IActionResult Details(Guid? id)
-    {
-        return View();
-    }
-
-    // GET: /Events/Create
+    [HttpGet]
     public async Task<IActionResult> Create()
     {
         var userIdStr = User.FindFirstValue(ClaimTypes.NameIdentifier);
@@ -108,11 +117,5 @@ public class EventsController : Controller
         }
 
         return RedirectToAction(nameof(Index));
-    }
-
-    // GET: /Events/Edit/5
-    public IActionResult Edit(Guid? id)
-    {
-        return View();
     }
 }
