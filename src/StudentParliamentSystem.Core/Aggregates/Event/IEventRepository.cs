e157ebae427999106fd6b17fca8b81b42e7df827
pichus <dimatyzuk@gmail.com>
2026-04-15 21:38:15 +0300

    feat(core): registration functionality

--- src/StudentParliamentSystem.Core/Aggregates/Event/IEventRepository.cs
+++ src/StudentParliamentSystem.Core/Aggregates/Event/IEventRepository.cs
@@ -21,4 +21,7 @@ public interface IEventRepository
         CancellationToken cancellationToken = default);
     Task DeleteAsync(Event @event, CancellationToken cancellationToken = default);
     Task<IEnumerable<Event>> GetByCreatedByUserAsync(Guid userId, CancellationToken cancellationToken = default);
+    Task<Event?> GetByIdWithRegistrationsAsync(Guid id, CancellationToken cancellationToken = default);
+    Task AddRegistrationAsync(EventRegistration registration, CancellationToken cancellationToken = default);
+    Task RemoveRegistrationAsync(EventRegistration registration, CancellationToken cancellationToken = default);
 }
