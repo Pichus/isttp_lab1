b323e70ca9dc13adf3597de138a0cd1756c2b39f
pichus <dimatyzuk@gmail.com>
2026-04-20 09:23:09 +0300

    fix: fix migration errors

---
+++ src/StudentParliamentSystem.Infrastructure/Data/Migrations/20260420061448_FixMigrationErrors.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace StudentParliamentSystem.Infrastructure.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixMigrationErrors : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "DepartmentRoleId",
+                schema: "main_application",
+                table: "department_members",
+                type: "uuid",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "department_roles",
+                schema: "main_application",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Name = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_department_roles", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_department_members_DepartmentRoleId",
+                schema: "main_application",
+                table: "department_members",
+                column: "DepartmentRoleId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_department_members_department_roles_DepartmentRoleId",
+                schema: "main_application",
+                table: "department_members",
+                column: "DepartmentRoleId",
+                principalSchema: "main_application",
+                principalTable: "department_roles",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_department_members_department_roles_DepartmentRoleId",
+                schema: "main_application",
+                table: "department_members");
+
+            migrationBuilder.DropTable(
+                name: "department_roles",
+                schema: "main_application");
+
+            migrationBuilder.DropIndex(
+                name: "IX_department_members_DepartmentRoleId",
+                schema: "main_application",
+                table: "department_members");
+
+            migrationBuilder.DropColumn(
+                name: "DepartmentRoleId",
+                schema: "main_application",
+                table: "department_members");
+        }
+    }
+}
